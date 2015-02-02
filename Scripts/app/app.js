3bba5c86d8ca7fa8672bb2138b925b417d3702f7
Dipen Shah <dipen08it419@gmail.com>
2015-02-02 06:05:29 -0800

    Test

---
+++ Scripts/app/app.js
@@ -0,0 +1,36 @@
+﻿
+//File: app.js
+//Author: Dipen Shah
+//Description: This script contains javascript code to register application module and to handle various paths of the web app.
+
+
+/// <reference path="../lib/angular.js" />
+$home = "";
+var app = angular.module('portfolioApp', ['ngRoute'])
+            .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+                $routeProvider.when('/', {
+                    templateUrl: 'partials/home.html',
+                    $home:'home'
+                }).when('/about', {
+                    templateUrl: 'partials/about.html'
+                }).when('/contact', {
+                    templateUrl: 'partials/contact.html',
+                    controller: 'ContactController'
+                }).when('/projects', {
+                    templateUrl: 'partials/projects.html'
+                }).when('/services', {
+                    templateUrl: 'partials/services.html'
+                })
+                .when('/termsOfUse', {
+                    templateUrl: 'partials/terms.html'
+                })
+                .when('/privacyPolicy', {
+                    templateUrl: 'partials/privacy.html'
+                })
+                .when('/sitemap', {
+                    templateUrl: 'partials/sitemap.html'
+                })
+                .otherwise({ redirectTo: '/' });
+
+                $locationProvider.html5Mode({ enabled: true, requireBase: false });
+            }]);
---
+++ Scripts/app/controllers.js
@@ -0,0 +1,37 @@
+﻿//File: app.js
+//Author: Dipen Shah
+//Description: This script contains javascript code to register various controller for the portfolio app.
+
+/// <reference path="../lib/angular.min.js" />
+
+var app = angular.module('portfolioApp');
+
+app.controller('ContactController', ['$scope','$http', function ($scope, $http) {
+    $scope.SendMessage = function () {
+        if ($scope.emailId && $scope.message) {
+            $http.post(
+                'AppCode/ContactService.asmx/SaveContact',
+                    {
+                        emailId: $scope.emailId,
+                        message: $scope.message
+                    }
+            ).success(function (data, status, header, config) {
+                alert('Your message sent successfully!');
+
+                $scope.contactForm.$setPristine();
+                $scope.emailId = "";
+                $scope.message = "";
+            })
+            .error(function (data, status, header, config) {
+                alert('Sorry! Unable to send message, please try again.');
+            });
+        } else {
+            alert('Please fill up the form.');
+        }
+    }
+}])
+.controller('NavigationController', ['$scope','$location',function ($scope, $location) {
+    $scope.isActive = function (path) {
+        return path === $location.path();
+    }
+}]);
